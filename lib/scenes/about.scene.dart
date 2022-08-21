8897aa010448bff28e98c692098b9b06c1496ce6
Ya'suf Dany <yasfdany@gmail.com>
2022-08-21 17:41:21 +0700

    Center the about page

--- lib/scenes/about.scene.dart
+++ lib/scenes/about.scene.dart
@@ -20,83 +20,86 @@ class _AboutScreenState extends State<AboutScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Themes.primary,
-      body: SizedBox(
-        width: kIsWeb ? 441 : MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                RippleButton(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Icon(
-                    Icons.chevron_left_rounded,
-                    size: 32,
+      body: Center(
+        child: SizedBox(
+          width: kIsWeb ? 441 : MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RippleButton(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Icon(
+                      Icons.chevron_left_rounded,
+                      size: 32,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            PopButton(
-              padding: EdgeInsets.zero,
-              onTap: () {},
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.asset(
-                  AssetImages.dany,
-                  width: 100,
-                  height: 100,
-                ),
+                ],
               ),
-            ).addSymmetricMargin(
-              vertical: 32,
-            ),
-            Text(
-              "Hello, I'm the Dev",
-              style: Themes().blackBold26,
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              "I know I know, you don't care\nmuch about it",
-              style: Themes().black16,
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              "I create this page only for formality, honestly I don't even know what I want to say to you but, thanks for playing my game cheers!\n\nI will just leave my social media here so you can eee... I don't know, Follow me I guess or just hit me on direct message",
-              style: Themes().black16,
-              textAlign: TextAlign.center,
-            ).addAllMargin(24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                PopButton(
-                  onTap: () {
-                    launchUrl(Uri.parse("https://www.instagram.com/yasfdany/"));
-                  },
-                  padding: EdgeInsets.zero,
+              PopButton(
+                padding: EdgeInsets.zero,
+                onTap: () {},
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
                   child: Image.asset(
-                    AssetImages.instagram,
-                    width: 32,
-                    height: 32,
+                    AssetImages.dany,
+                    width: 100,
+                    height: 100,
                   ),
                 ),
-                Container(width: 24),
-                PopButton(
-                  onTap: () {
-                    launchUrl(Uri.parse("https://twitter.com/yasfdany"));
-                  },
-                  padding: EdgeInsets.zero,
-                  child: Image.asset(
-                    AssetImages.twitter,
-                    width: 32,
-                    height: 32,
+              ).addSymmetricMargin(
+                vertical: 32,
+              ),
+              Text(
+                "Hello, I'm the Dev",
+                style: Themes().blackBold26,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "I know I know, you don't care\nmuch about it",
+                style: Themes().black16,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "I create this page only for formality, honestly I don't even know what I want to say to you but, thanks for playing my game cheers!\n\nI will just leave my social media here so you can eee... I don't know, Follow me I guess or just hit me on direct message",
+                style: Themes().black16,
+                textAlign: TextAlign.center,
+              ).addAllMargin(24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  PopButton(
+                    onTap: () {
+                      launchUrl(
+                          Uri.parse("https://www.instagram.com/yasfdany/"));
+                    },
+                    padding: EdgeInsets.zero,
+                    child: Image.asset(
+                      AssetImages.instagram,
+                      width: 32,
+                      height: 32,
+                    ),
                   ),
-                ),
-              ],
-            ).addMarginTop(24),
-          ],
+                  Container(width: 24),
+                  PopButton(
+                    onTap: () {
+                      launchUrl(Uri.parse("https://twitter.com/yasfdany"));
+                    },
+                    padding: EdgeInsets.zero,
+                    child: Image.asset(
+                      AssetImages.twitter,
+                      width: 32,
+                      height: 32,
+                    ),
+                  ),
+                ],
+              ).addMarginTop(24),
+            ],
+          ),
         ),
       ),
     );
